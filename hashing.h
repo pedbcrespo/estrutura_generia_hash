e14a723ef013a11bbcd70a525190d0d6a535fc5d
pedbcrespo <55194763+pedbcrespo@users.noreply.github.com>
2021-01-04 12:17:34 -0300

    Update hashing.h

--- hashing.h
+++ hashing.h
@@ -11,7 +11,7 @@ void imprimir(tbl tab);
 /*Para rodar o codigo:
     -abra o terminal (cmd caso seja o windows)
     -acesse a pasta do projeto pelo terminal
-    -digite (caso queira rodar a main1.c): gcc main1.c hashing.c -o main1.exex
+    -digite (caso queira rodar a main1.c): gcc main1.c hashing.c -o main1.exe
     -digite (caso queira rodar a main2.c): gcc main2.c hashing.c -o main2.exe
     -caso queira rodar seu proprio arquivo.c: gcc [nome_do_seu_arquivo].c hashing.c -o [nome_de_saida].exe
         -ps: lembrar de importar no seu arquivo, a biblioteca "hashing.h"
